2dac988b643ea01459ef07cb3d80a018ade8fbab
shalaka-kulkarni <k.shalaka96@gmail.com>
2021-05-08 21:07:02 -0400

    class complete, SVM doesn't work, need to debug model execution

--- models/word_frequency_model.py
+++ models/word_frequency_model.py
@@ -1,4 +1,4 @@
-from models.abstract_model import AbstractModel
+from abstract_model import AbstractModel
 import re
 import string
 import pandas as pd
@@ -24,13 +24,13 @@ class BOW(AbstractModel):
         return wdlist
 
     ### Take author and input text, return df (Series) of profiles
-    def _makeProfile(auth, text):
+    def _makeProfile(self, auth, text):
         wdlist = _preprocess(text)
 
         totalvocab = len(wdlist)
 
         fd = FreqDist(wdlist)
-        commonwords = dict(fd.most_common(topN))
+        commonwords = dict(fd.most_common(self.topN))
         df = pd.DataFrame.from_dict(commonwords, orient='index', columns=[auth])
         df = df/totalvocab #normalize
         df = df.transpose()
@@ -38,26 +38,70 @@ class BOW(AbstractModel):
         return df
 
     ### Convert text to features
-    def _makeFeatureMatrix(training_data, topN):
+    def _makeFeatureMatrix(self, training_data):
         featdf = pd.DataFrame()
+
         for auth in training_data:
             text = training_data[auth]
-            df = _makeProfile(auth, text)
+
+            df = _makeProfile(self, auth, text)
+
             featdf = featdf.append(df) #, ignore_index=True)
 
         featdf.fillna(0, inplace=True)
+
+        #print(featdf)
         return featdf
 
     ## Measures the similarity between two profiles by the angle formed between them ##
     def _cosSimilarity(p1, p2):
         return (p1 @ p2) / (np.linalg.norm(p1) * np.linalg.norm(p2))
     
-    def train(self, training_data):
-        ##TODO: call _makeFeatureMatrix and convert to desired data structure of o/p profiles
-        pass
+    def train(self, training_data, topN=100):
+        self.topN = topN
+
+        traindict = {}
+        for auth in training_data:
+            corpus = ''
+            for text in training_data[auth]:
+                corpus = corpus+text+' '
+            traindict[auth] = corpus
+
+        featmatrix = _makeFeatureMatrix(self, traindict)
+
+        self.profiledf = featmatrix
+        return
+            
+    def _createTest(self, text):
+    
+        featmatrix = self.profiledf
+
+        pr = _makeProfile(self, 'Test', text)
+
+        testdf = featmatrix.append(pr)
+        testdf.fillna(0, inplace=True)
+
+        col_list = featmatrix.columns.tolist()
+        testdf = testdf.iloc[-1]
+
+        return testdf[col_list]    
         
     def identify(self, text):
-        ##TODO: call _makeProfile on text, find cosSimilarity, convert to probability
-        pass
+    
+        testdf = _createTest(self, text)
+        authors = self.profiledf.index.tolist()
+
+        M = len(self.profiledf)
+        clist = []
+        for i in range(M):
+            clist.append(cosSimilarity(self.profiledf.iloc[i], testdf.iloc[0]))
+
+        probdict = {}
+        for i in range(M):
+            probdict[authors[i]] = clist[i]/sum(clist)
+
+        idlist = sorted([(clist[i]/sum(clist), authors[i]) for i in range(M)], reverse=True)
+
+        return idlist
 
 

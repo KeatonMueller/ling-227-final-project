58efdda4d8481e411d43f50cc5084fb060fc7594
dovgreenwood <dov.greenwood@gmail.com>
2021-05-12 16:51:59 -0400

    updated Project.ipynb (weights, ensemble model test) and fixed ensemble_model.py

--- models/ensemble_model.py
+++ models/ensemble_model.py
@@ -4,12 +4,23 @@ from models.compression_model import CompressionModel
 from models.word_frequency_model import BOW
 
 class Ensemble(AbstractModel):
-    def __init__(self, weights=(1/3.0, 1/3.0, 1/3.0), CNGM_specs=(2, 0), BOW_topN=100):
-        assert len(CNGM_specs) == 2
+    def __init__(self, weights=(1/3.0, 1/3.0, 1/3.0), CNGM_specs=(2, 0, False), BOW_topN=100):
+        """
+        weights should be a tuple contained the weights for the models, in the following order:
+                (BOW weight, CompressionModel weight, CNGM weight)
+        default is 1/3 for each.
         
+        CNGM_specs should be a tuple containing the specs of the CNGM model in the same order
+        as specified in the CNGM class; default is the same as CNGM defaults.
+        
+        BOW_topN should be an int following the specification of the BOW class.
+        """
+        assert len(CNGM_specs) == 3
+        
+        self.weights = weights
         self.bowm = BOW(BOW_topN)
         self.comm = CompressionModel()
-        self.cngm = CNGM(CNGM_specs[0], CNGM_specs[1])
+        self.cngm = CNGM(CNGM_specs[0], CNGM_specs[1], CNGM_specs[2])
 
 
     def train(self, training_data):
@@ -26,7 +37,7 @@ class Ensemble(AbstractModel):
         cng_probs = self.cngm.identify(text)
         cng_probs = sorted(cng_probs, key=lambda p: p[1])
 
-        probs = [( (weights[0]*bow_probs[i][0])
-                    + weights[1]*com_probs[i][0]
-                    + weights[2]*cng_probs[i][0], bow_probs[i][1] ) for i in range(len(bow_probs))]
+        probs = [( (self.weights[0]*bow_probs[i][0])
+                    + self.weights[1]*com_probs[i][0]
+                    + self.weights[2]*cng_probs[i][0], bow_probs[i][1] ) for i in range(len(bow_probs))]
         return sorted(probs, reverse=True)
